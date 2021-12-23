0264756ea55f26fc9e40b230719656d3caac8cd0
Antonio Vizuete <antoniovizuete@gmail.com>
2021-12-23 07:49:23 +0100

    Add non-recursive way to 22.

--- day22/index.js
+++ day22/index.js
@@ -1,5 +1,23 @@
 //Day 22
 export default function countDecorations(bigTree) {
+  let node = bigTree;
+  let toVisit = [];
+  let sum = 0
+  
+  while (!!node || toVisit.length > 0) {
+    while (!!node) {
+      toVisit.push(node);
+      node = node.left
+    }
+    
+    node = toVisit.pop();
+    sum += node.value;
+    node = node.right;
+  }
+  return sum;
+}
+
+function recursiveCountDecorations(bigTree) {
   let sum = 0;
   function traverse(node) {
     node.left && traverse(node.left);
