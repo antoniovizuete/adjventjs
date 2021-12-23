7b8fdbf5bcb5c0da255ce57e06c0ffe6fb0294da
Antonio Vizuete <antoniovizuete@gmail.com>
2021-12-23 13:14:30 +0100

    Update 22

--- day21/index.js
+++ day21/index.js
@@ -1,9 +1,30 @@
 //Day 21
 export default function canCarry(capacity, trip) {
+  const getCollectData = (a, c) => [...a, [c[1], c[0]]];
+  const getDeliveryData = (a, c) => [...a, [c[2], -c[0]]];
+  
+  const plannedTrip = [
+    ...trip.reduce(getCollectData, []),
+    ...trip.reduce(getDeliveryData, []),
+  ];
+  
+  const sortByLoctionAndQuantity = ([la, qa], [lb, qb]) => la - lb === 0 ? qa - qb : la - lb;
+  const toExceeding = (a, [,q]) => (a += q) > capacity ? Infinity : a;
+  
+  const exceedsMaxLoad = !plannedTrip
+    .sort(sortByLoctionAndQuantity)
+    .reduce(toExceeding, 0);
+  
+  return exceedsMaxLoad;
+}
+
+export default function canCarry(capacity, trip) {
+  const sortByLoctionAndQuantity = ([la, qa], [lb, qb]) => la - lb === 0 ? qa - qb : la - lb;
+
   const planned = [
     ...trip.reduce((a, c) => [...a, [c[1], c[0]]], []),
     ...trip.reduce((a, c) => [...a, [c[2], -c[0]]], []),
-  ].sort(([la, qa], [lb, qb]) => la - lb === 0 ? qa - qb : la - lb);
+  ].sort(sortByLoctionAndQuantity);
 
   let current = 0;
   for (const step of planned) {
